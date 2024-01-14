cfb91128722d227fbf2750f644b09760fe97f3ab
mStar aka a person <12024604-mstarongitlab@users.noreply.gitlab.com>
2024-01-14 15:13:30 +0100

    Update tower and added util autoload Tower now snaps to the center of the nearest TileMap cell while dragging in editor (using a function in Util)

---
+++ Globals/Util.gd
@@ -0,0 +1,25 @@
+@tool
+extends Node
+
+## Snaps the given node to the center of the closest tile on a tilemap in editor
+## Note: Currently only searches the topmost layer of the currently open scene (all the children of the root node)
+func snap_to_tilemap(node_to_snap: Node2D) -> void:
+	var tilemap: TileMap
+	for child in EditorInterface.get_edited_scene_root().get_children():
+		if child is TileMap:
+			tilemap = child as TileMap
+			break
+	# TODO: Optionally add deep search
+	if not tilemap:
+		return
+	# And then convert it back to global and set own global to that
+	node_to_snap.global_position = tilemap.to_global(
+		# Turn that into local space to center
+		tilemap.map_to_local(
+			# Then get the map tile position
+			tilemap.local_to_map(
+				# Turn it into a local position for the tilemap
+				tilemap.to_local(
+					# Take own global positon
+					node_to_snap.global_position
+	))))
--- TowerDefence/Tower.gd
+++ TowerDefence/Tower.gd
@@ -1,3 +1,4 @@
+@tool
 class_name Tower extends Node2D
 
 @export var config: TowerData
@@ -5,7 +6,29 @@ class_name Tower extends Node2D
 ## Does not overwrite health handling
 @export var custom_behaviour: CustomTowerBehaviour
 
-@onready var detection_shape: CollisionShape2D = $Area2D/CollisionShape2D
+@onready var _detection_shape: CollisionShape2D = $Area2D/CollisionShape2D
+@onready var _sprite_2d: Sprite2D = $Sprite2D
 
 func _ready() -> void:
-	(detection_shape.shape as CircleShape2D).radius = config.range
+	if Engine.is_editor_hint():
+		_ready_engine()
+	else:
+		_ready_game()
+
+func _process(delta: float) -> void:
+	if Engine.is_editor_hint():
+		_process_editor(delta)
+	else:
+		_process_game(delta)
+
+func _process_editor(_delta: float) -> void:
+	Util.snap_to_tilemap(self)
+
+func _process_game(_delta: float) -> void:
+	pass
+
+func _ready_engine() -> void:
+	pass
+	
+func _ready_game() -> void:
+	(_detection_shape.shape as CircleShape2D).radius = config.range
