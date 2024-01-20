90368d2823682f2177b40fa1f837e676f43d269b
mStarOnGithub <30903890+mstarongithub@users.noreply.github.com>
2024-01-20 21:21:49 +0100

    Stupid preload thing doing stupid things

--- UI/AssetsInfo.gd
+++ UI/AssetsInfo.gd
@@ -1,6 +1,4 @@
 extends Control
 
-const MAIN = preload("res://UI/main.tscn")
-
 func _on_return_pressed() -> void:
-	get_tree().change_scene_to_packed(MAIN)
+	get_tree().change_scene_to_file("res://UI/main.tscn")
--- UI/Main.gd
+++ UI/Main.gd
@@ -1,10 +1,10 @@
 extends MarginContainer
 
-const ASSETS_INFO = preload("res://UI/AssetsInfo.tscn")
+const SOURCES = preload("res://UI/Sources.tscn")
 const GAME_SCENE_FRAMEWORK = preload("res://GameSceneFramework.tscn")
 
 func _on_sources_pressed() -> void:
-	get_tree().change_scene_to_packed(ASSETS_INFO)
+	get_tree().change_scene_to_packed(SOURCES)
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_packed(GAME_SCENE_FRAMEWORK)
